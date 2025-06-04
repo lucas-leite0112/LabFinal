60ab064bca3e5259c09d0f1a3ba733c37ecc6d5d
Lucas Leite <lukinhas.leite2011@gmail.com>
2025-06-04 20:14:40 -0300

    criação de novos testes

--- test_app.py
+++ test_app.py
@@ -1,6 +1,7 @@
 import unittest
 from app import app
 import werkzeug
+import json
 
 # Patch temporário para adicionar o atributo '__version__' em werkzeug
 if not hasattr(werkzeug, '__version__'):
@@ -17,14 +18,25 @@ class APITestCase(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json, {"message": "API is running"})
 
-    def test_login(self):
+    def test_get_items(self):
+        response = self.client.get('/items')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json, {"items": ["item1", "item2", "item3"]})
+        self.assertIsInstance(response.json['items'], list)
+        self.assertGreater(len(response.json['items']), 0)
+
+
+    def test_login_success(self):
         response = self.client.post('/login')
         self.assertEqual(response.status_code, 200)
         self.assertIn('access_token', response.json)
+        self.login_token = response.json['access_token']
 
-    def test_protected_no_token(self):
+    def test_protected_route_access_denied(self):
         response = self.client.get('/protected')
         self.assertEqual(response.status_code, 401)
+        self.assertEqual(response.json, {"message": "Token is missing or invalid"})
+
 
 if __name__ == '__main__':
     unittest.main()
